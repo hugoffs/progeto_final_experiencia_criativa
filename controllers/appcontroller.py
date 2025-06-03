1fca8086fcf8e8324e39835a04ba02af810d0735
hugoffs <hugofagunfs@gmail.com>
2025-06-03 15:04:01 -0300

    adição dos arquivos registrar_novo_usuario.html

--- controllers/appcontroller.py
+++ controllers/appcontroller.py
@@ -3,10 +3,6 @@ from flask import Flask, render_template, request
 def create_app():
     app = Flask(__name__, template_folder="./views", static_folder="./static", root_path="./")
 
-    @app.route('/')
-    def index():
-        return render_template('login.html')
-
     @app.route('/login', methods=['POST'])
     def login():
         username = request.form.get('username')
@@ -19,5 +15,11 @@ def create_app():
     @app.route("/recuperar_conta")
     def recuperar_conta():
         return render_template('recuperar_conta.html')
+
+    # função onde inicia o app
+    @app.route('/')
+    def index():
+        return render_template('registrar_usuario.html')
+    
 
     return app
